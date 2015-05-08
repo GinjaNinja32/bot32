83b3db64cc425218659d765de181958652fe7e9b
GinjaNinja32 <ginjaninja32@gmail.com>
2015-05-08 23:30:44 +0000

    Things.

--- flatten.erl
+++
@@ -1,9 +0,0 @@
--module(flatten).
--compile(export_all).
-
-flatten(L) -> lists:reverse(flatten(L, [])).
-
-flatten([], Out) -> Out;
-flatten([A|B], Out) when is_list(A) -> flatten(B, flatten(A, Out));
-flatten([A|B], Out) -> flatten(B, [A | Out]);
-flatten(A, Out) -> [A | Out].
