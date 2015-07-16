95b71c883ac644ed22226da7e2c8dbde7f25845e
GinjaNinja32 <ginjaninja32@gmail.com>
2015-07-16 20:21:50 +0100

    Bumps version number to 0.3

--- definitions.hrl
+++ definitions.hrl
@@ -1,5 +1,5 @@
 
--define(VERSION, "Bot32 v0.2").
+-define(VERSION, "Bot32 v0.3").
 
 -record(state, {nick, prefix, permissions, ignore, commands, moduledata, modules}).
 -record(user, {nick, username, host}).
