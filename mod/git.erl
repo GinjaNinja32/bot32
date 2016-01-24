ce392ec28646d4b98dec71403602c9350d737c90
GinjaNinja32 <ginjaninja32@gmail.com>
2016-01-24 08:35:44 +0000

    Move defines into git module

--- mod/git.erl
+++ mod/git.erl
@@ -6,65 +6,81 @@
 get_commands() ->
 	[
 		{"search", fun search/1, user},
-		{"dump_git", fun dump/1, host},
-		{"reload_git", fun reload/1, admin}
+		{"reload_git", fun reload/1, admin},
+		{"defined", fun defined/1, user}
 	].
 
 initialise() ->
-	config:set_value(temp, [?MODULE], load_trees()).
+	load_trees().
 deinitialise() ->
 	config:del_value(temp, [?MODULE]).
 
 %
 
-search(#{reply:=RT, ping:=Ping, params:=[]}) -> {irc, {msg, {RT, [Ping, "Please provide a search term."]}}};
-search(#{reply:=RT, ping:=Ping, params:=Params}) ->
-	{M,DF,D,KP} = config:require_value(temp, [?MODULE]),
-	{Tree,Branch,UseParams} = case string:to_lower(hd(Params)) of
-		"master" -> {M, "master", tl(Params)};
-		"dev" -> {D, "dev", tl(Params)};
-		"dev-freeze" -> {DF, "dev-freeze", tl(Params)};
-		"devfreeze" -> {DF, "dev-freeze", tl(Params)};
-		"freeze" -> {DF, "dev-freeze", tl(Params)};
-		"kunpeng" -> {KP, "kunpeng", tl(Params)};
-		"kun-peng" -> {KP, "kunpeng", tl(Params)};
-		_ -> {M, "master", Params}
-	end,
-	case UseParams of
-		[] -> {irc, {msg, {RT, [Ping, "Please provide a search term."]}}};
+branch_params(Params, Type) ->
+	case Params of
+		[X] -> {"master", [X]};
+		[] -> error;
 		_ ->
-			SearchString = string:to_lower(string:join(UseParams, " ")),
-			{irc, {msg, {RT, search_tree(Tree, SearchString, Branch)}}}
+			case config:get_value(temp, [?MODULE, Type, string:to_lower(hd(Params))]) of
+				'$none' -> error;
+				_ -> {string:to_lower(hd(Params)), tl(Params)}
+			end
 	end.
 
-dump(#{reply:=RT, ping:=P}) ->
-	{M,DF,D,KP} = config:require_value(temp, [?MODULE]),
-	file:write_file("git-master.crl", io_lib:format("~p.~n", [M])),
-	file:write_file("git-devfreeze.crl", io_lib:format("~p.~n", [DF])),
-	file:write_file("git-dev.crl", io_lib:format("~p.~n", [D])),
-	file:write_file("git-kunpeng.crl", io_lib:format("~p.~n", [KP])),
-	{irc, {msg, {RT, [P, "Dumped file trees to text."]}}}.
+search(#{reply:=Reply, ping:=Ping, params:=Params}) ->
+	case branch_params(Params, trees) of
+		error -> {irc, {msg, {Reply, [Ping, "Provide a valid branch to search!"]}}};
+		{BR, PR} ->
+			SearchString = string:to_lower(string:join(PR, " ")),
+			{irc, {msg, {Reply, [Ping, search_tree(BR, SearchString)]}}}
+	end.
 
 reload(#{reply:=ReplyTo, ping:=Ping}) ->
-	config:set_value(temp, [?MODULE], load_trees()),
+	load_trees(),
 	{irc, {msg, {ReplyTo, [Ping, "Reloaded Git file tree"]}}}.
+
+defined(#{reply:=Reply, ping:=Ping, params:=Params}) ->
+	case branch_params(Params, defines) of
+		error -> {irc, {msg, {Reply, [Ping, "Provide a valid branch to search!"]}}};
+		{BR, PR} ->
+			Def = hd(PR),
+			case config:get_value(temp, [?MODULE, defines, BR, Def]) of
+				'$none' -> {irc, {msg, {Reply, [Ping, "No define of '", Def, "' found."]}}};
+				{File, Val} -> {irc, {msg, {Reply, [Ping, "'", Def, "' is defined as ", Val, " in ", File, $.]}}}
+			end
+	end.
+
 
 %
 
 load_trees() ->
 	update(),
-	Master=load_tree("master"),
-	Dev=load_tree("dev"),
-	DevFreeze=load_tree("dev-freeze"),
-	Kunpeng=load_tree("kunpeng"),
-	{Master, DevFreeze, Dev, Kunpeng}.
+	lists:foreach(fun(T) ->
+			config:set_value(temp, [?MODULE, trees, T], load_tree(T)),
+			config:set_value(temp, [?MODULE, defines, T], load_defines(T))
+		end, ["master", "dev-freeze", "dev", "kunpeng"]).
+
+load_defines(Branch) ->
+	Home = config:require_value(config, [?MODULE, location]),
+	Remote = config:get_value(config, [?MODULE, remote], "upstream"),
+	MatchingLines = string:tokens(util:safe_os_cmd(["cd ", Home, "; git grep '#define' ", Remote, "/", Branch]), "\r\n"),
+	lists:keysort(1, lists:filtermap(fun(Line) ->
+			% origin/dev:code/setup.dm:#define FOO 2
+			case re:run(Line, "[^:]+:([^:]+):#define\\s+(\\S+)\\s+(.*)", [{capture,all_but_first,list}]) of
+				{match, [File, Def, Val]} ->
+					{true, {Def, {File, Val}}};
+				nomatch -> false
+			end
+		end, MatchingLines)).
 
 load_tree(Branch) ->
 	Home = config:require_value(config, [?MODULE, location]),
 	Remote = config:get_value(config, [?MODULE, remote], "upstream"),
-	string:tokens(os:cmd(["cd ",Home,"; git ls-files --with-tree ",Remote,"/", Branch]), "\r\n").
+	string:tokens(util:safe_os_cmd(["cd ",Home,"; git ls-files --with-tree ",Remote,"/", Branch]), "\r\n").
 
-search_tree(Tree, String, Branch) ->
+search_tree(Branch, String) ->
+	Tree = config:require_value(temp, [?MODULE, trees, Branch]),
 	case lists:filter(fun(T) ->
 				string:str(string:to_lower(T), String) /= 0
 			end, Tree) of
