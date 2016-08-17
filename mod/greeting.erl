38c8b2757fb648f9efe7da594c3d941f75cc08f0
GinjaNinja32 <ginjaninja32@gmail.com>
2016-08-17 16:39:00 +0100

    Merge pull request #17

---
+++ mod/greeting.erl
@@ -0,0 +1,42 @@
+-module(greeting).
+-compile(export_all).
+
+-include("definitions.hrl").
+
+-define(OPTS, [global, {return, binary}]).
+
+filter(Msg) ->
+	M1 = util:bin_to_lower(Msg), % Lowercase
+	M2 = re:replace(M1, "[\r\n'\"]+", "", ?OPTS), % Remove junk we don't care about
+	M3 = re:replace(M2, "\\((.*?)\\)", "\\1", ?OPTS), % |
+	M4 = re:replace(M3, "\\[(.*?)\\]", "\\1", ?OPTS), % | Remove *matched* brackets
+	M5 = re:replace(M4, "\\{(.*?)\\}", "\\1", ?OPTS), % |
+	M6 = re:replace(M5, "<(.*?)>",     "\\1", ?OPTS), % |
+	M7 = re:replace(M6, "https?://\\S*", "", ?OPTS), % Remove URLs
+	M8 = re:replace(M7, "\\.[\\.\\s]*\\.", "...", ?OPTS), % Replace strings of ..... and spaces with just three
+	re:replace(M8, "\s\s+", " ", ?OPTS). % Replace multiple spaces with just one
+
+do_extras(Tokens, Reply, Ping) ->
+	String = filter(list_to_binary(string:join(Tokens, " "))),
+	Nick = util:regex_escape(config:require_value(config, [bot, nick])),
+	Aliases = lists:map(fun util:regex_escape/1, config:require_value(config, [bot,names])),
+	case re:run(String, ["\\b(", util:binary_join([Nick|Aliases],<<"|">>), ")s?\\b"], [{capture, none}, caseless]) of
+		match ->
+			case re:run(String, "\\b(good ?)?night\\b", [{capture, none}]) of
+				match -> core ! {irc, {msg, {Reply, [Ping, "Goodnight!"]}}};
+				nomatch ->
+					case re:run(String, "\\bmorning\\b", [{capture, none}]) of
+						match -> core ! {irc, {msg, {Reply, [Ping, "Morning!"]}}};
+						nomatch ->
+							case re:run(String, "\\bh(ello|i)\\b", [{capture, none}]) of
+								match -> core ! {irc, {msg, {Reply, [Ping, "Hello!"]}}};
+								nomatch ->
+									case re:run(String, "\\b(good ?)?bye\\b", [{capture, none}]) of
+										match -> core ! {irc, {msg, {Reply, [Ping, "Goodbye!"]}}};
+										nomatch -> ok
+									end
+							end
+					end
+			end;
+		nomatch -> ok
+	end.
