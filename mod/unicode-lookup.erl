bc640d12efa2df73c75c52cb12b9abd11152f298
GinjaNinja32 <ginjaninja32@gmail.com>
2016-12-21 18:36:18 +0000

    Add unicode lookup

---
+++ mod/unicode-lookup.erl
@@ -0,0 +1,13 @@
+-module('unicode-lookup').
+-export([get_commands/0]).
+
+get_commands() ->
+	[
+		{"unicode", fun unicode/1, [short], admin}
+	].
+
+unicode(#{reply:=Reply, ping:=Ping, params:=[Codepoint]}) ->
+	CodeInt = list_to_integer(Codepoint, 16),
+	os:putenv("codepoint", Codepoint),
+	Msg = util:safe_os_cmd(<<"curl -so- \"https://codepoints.net/U+$codepoint\" | grep -Eo '<title>.* . Codepoints</title>' | sed -r 's|<title>||; s| . Codepoints</title>||'">>),
+	{irc, {msg, {Reply, [Ping, Msg, ": ", <<CodeInt/utf8>>]}}}.
